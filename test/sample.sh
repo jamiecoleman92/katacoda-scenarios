39944f74abd787ba16355d51ff008c738010e3d4
jamiecoleman92 <jlcoleman@uk.ibm.com>
2018-12-19 23:33:40 +0000

    remove change of dir in intro

--- test/sample.sh
+++ test/sample.sh
@@ -2,8 +2,6 @@
 
 mkdir test
 git clone https://github.com/jamiecoleman92/kube-java-tutorial.git
-cd kube-java-tutorial
 docker pull jamielcoleman/kubejavasample:ping
 docker pull jamielcoleman/kubejavasample:name
-
 
