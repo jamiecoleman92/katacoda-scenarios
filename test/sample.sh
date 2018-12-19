2223c32ecc584ee6b8f6e52cd8b0c53b5de0aed7
jamiecoleman92 <jlcoleman@uk.ibm.com>
2018-12-19 23:51:44 +0000

    Add kube demplyment yaml

--- test/sample.sh
+++ test/sample.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 mkdir test
-git clone https://github.com/jamiecoleman92/kube-java-tutorial.git
 docker pull jamielcoleman/kubejavasample:ping
 docker pull jamielcoleman/kubejavasample:name
 
