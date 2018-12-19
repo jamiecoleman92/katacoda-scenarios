e7572f95751aaccbcc77d8ae17157b23e1116736
jamiecoleman92 <jlcoleman@uk.ibm.com>
2018-12-19 22:49:46 +0000

    Add test mkdir command to start of script

--- test/sample.sh
+++ test/sample.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+mkdir test
 git clone git@github.com:jamiecoleman92/kube-java-tutorial.git
 docker pull jamielcoleman/kubejavasample:ping
 docker pull jamielcoleman/kubejavasample:name
