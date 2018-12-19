ff7d6c32a41d3d921fa784ab4764e9527e54a527
jamiecoleman92 <jlcoleman@uk.ibm.com>
2018-12-19 22:52:38 +0000

    add https pull method to git pull command

--- test/sample.sh
+++ test/sample.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir test
-git clone git@github.com:jamiecoleman92/kube-java-tutorial.git
+git clone https://github.com/jamiecoleman92/kube-java-tutorial.git 
 docker pull jamielcoleman/kubejavasample:ping
 docker pull jamielcoleman/kubejavasample:name
 cd kube-java-tutorial
