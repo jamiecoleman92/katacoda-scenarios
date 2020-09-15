636a51782de1323bdad85ad45fa8e30a6751e4f3
Jamie L Coleman <jlcoleman@uk.ibm.com>
2020-09-15 15:03:04 +0100

    Attempt 2 to fix path for JVM

--- kubeconfig/pre.sh
+++ kubeconfig/pre.sh
@@ -3,4 +3,4 @@
 git clone https://github.com/jamiecoleman92/guide-kubernetes-microprofile-config.git
 docker pull open-liberty:latest
 
-export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/
+JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/
