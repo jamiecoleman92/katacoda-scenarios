dcb56b5fde9bccb3808f69957b69ca5911ae48f3
Jamie <jlcoleman@uk.ibm.com>
2020-10-03 00:51:10 +0100

    test new structure

--- kubeconfig/assets/env-init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/jamiecoleman92/guide-kubernetes-microprofile-config.git
-docker pull open-liberty:latest
-
-export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
-
-ssh root@host01 "while [ \`minikube status &>/dev/null; echo \$?\` -ne 0 ]; do sleep 1; done && kubectl run kubernetes-bootcamp --image=gcr.io/google-samples/kubernetes-bootcamp:v1 --port=8080 && echo done >> /opt/katacoda-completed"
