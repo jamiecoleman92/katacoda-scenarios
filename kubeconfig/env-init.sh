80ed6feacfacd0b714c4981d0a1a9543e098a6a8
Jamie <jlcoleman@uk.ibm.com>
2020-10-03 01:08:03 +0100

    Make changes to backend env

--- kubeconfig/env-init.sh
+++ kubeconfig/env-init.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 git clone https://github.com/jamiecoleman92/guide-kubernetes-microprofile-config.git
 docker pull open-liberty:latest
 
--- kubeconfig/foreground.sh
+++ kubeconfig/foreground.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/
 
 sleep 1; launch.sh
